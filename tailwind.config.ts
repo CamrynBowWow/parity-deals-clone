8978e577113d3c00b2e2a22c8e48df5aaf4ca099
CamrynBowWow <bowdencamryn@gmail.com>
2024-12-25 21:47:45 +0200

    Startup Done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: '1.5rem',
+				screens: { sm: '100%', md: '100%', lg: '100%', xl: '1200px' },
+			},
+			fontFamily: {
+				sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+				mono: ['var(--font-geist-sans', ...fontFamily.mono],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
