3c616aececbb8c60976d3f3320c2f127e0ccca78
CamrynBowWow <bowdencamryn@gmail.com>
2025-01-18 09:10:47 +0200

    Type Safe Environment Variables Done

---
+++ src/data/env/server.ts
@@ -0,0 +1,20 @@
+// Gives me type safe access to all the environment variables
+
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+	emptyStringAsUndefined: true,
+	server: {
+		DATABASE_URL: z.string().url(),
+		CLERK_SECRET_KEY: z.string(),
+	},
+	experimental__runtimeEnv: process.env, // Hooks up everything for me
+
+	/* 
+        If using 'runtimeEnv' then you will need to manually to add the values
+    */
+	// runtimeEnv: {
+	//     DATABASE_URL: process.env.DATABASE_URL
+	// }
+});
